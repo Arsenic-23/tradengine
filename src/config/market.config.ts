37ae20b04628eac8003c5bd5a10a39a579bff185
Arsenic <156888342+Arsenic-23@users.noreply.github.com>
2025-07-29 15:59:38 +0530

    Update market.config.ts

--- src/config/market.config.ts
+++ src/config/market.config.ts
@@ -1,1 +1,6 @@
-
+export const marketConfig = () => ({
+  market: {
+    websocketUrl: process.env.MARKET_WEBSOCKET_URL,
+    pollingInterval: parseInt(process.env.MARKET_POLLING_INTERVAL, 10) || 10000,
+  },
+});
