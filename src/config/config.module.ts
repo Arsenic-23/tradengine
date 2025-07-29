ee1e59ced585c2cc0f126f68addbbb23e2f3f544
Arsenic <156888342+Arsenic-23@users.noreply.github.com>
2025-07-29 15:59:08 +0530

    Update config.module.ts

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,1 +1,16 @@
-#
+import { Module } from '@nestjs/common';
+import { ConfigModule as NestConfigModule } from '@nestjs/config';
+import configuration from './config.service';
+import { databaseConfig } from './database.config';
+import { jwtConfig } from './jwt.config';
+import { marketConfig } from './market.config';
+
+@Module({
+  imports: [
+    NestConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration, databaseConfig, jwtConfig, marketConfig],
+    }),
+  ],
+})
+export class ConfigModule {}
