ba9d26ce3a5b7edc0fb1dd29e554db6aa288d4a0
Arsenic <156888342+Arsenic-23@users.noreply.github.com>
2025-07-30 20:27:54 +0530

    Update hash.util.ts

--- src/common/utils/hash.util.ts
+++ src/common/utils/hash.util.ts
@@ -1,1 +1,13 @@
+import * as bcrypt from 'bcrypt';
+import { PASSWORD_SALT_ROUNDS } from 'src/constants/app.constants';
 
+export const hashPassword = async (password: string): Promise<string> => {
+  return await bcrypt.hash(password, PASSWORD_SALT_ROUNDS);
+};
+
+export const comparePassword = async (
+  plainPassword: string,
+  hash: string,
+): Promise<boolean> => {
+  return await bcrypt.compare(plainPassword, hash);
+};
