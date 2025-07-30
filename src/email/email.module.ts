00ebf204e9ad6ac82f1602d319606ab1e64da83f
Arsenic <156888342+Arsenic-23@users.noreply.github.com>
2025-07-31 03:47:35 +0530

    Update email.module.ts

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -1,1 +1,38 @@
+import { Module } from '@nestjs/common';
+import { EmailService } from './email.service';
+import { ConfigModule } from '@nestjs/config';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { join } from 'path';
 
+@Module({
+  imports: [
+    ConfigModule,
+    MailerModule.forRootAsync({
+      useFactory: async () => ({
+        transport: {
+          host: process.env.SMTP_HOST,
+          port: parseInt(process.env.SMTP_PORT) || 587,
+          secure: false,
+          auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS,
+          },
+        },
+        defaults: {
+          from: `"Scalefund" <${process.env.SMTP_FROM}>`,
+        },
+        template: {
+          dir: join(__dirname, 'templates'),
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+    }),
+  ],
+  providers: [EmailService],
+  exports: [EmailService],
+})
+export class EmailModule {}
