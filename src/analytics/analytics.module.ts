b9e562c6c03d0af554ca834e4e4ab5524dab415f
Arsenic <156888342+Arsenic-23@users.noreply.github.com>
2025-07-31 03:49:12 +0530

    Update analytics.module.ts

--- src/analytics/analytics.module.ts
+++ src/analytics/analytics.module.ts
@@ -1,1 +1,21 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AnalyticsController } from './analytics.controller';
+import { AnalyticsService } from './analytics.service';
+import { Analytics, AnalyticsSchema } from './analytics.schema';
+import { UsersModule } from '../users/users.module';
+import { TradesModule } from '../trades/trades.module';
+import { PortfolioModule } from '../portfolio/portfolio.module';
 
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Analytics.name, schema: AnalyticsSchema }]),
+    UsersModule,
+    TradesModule,
+    PortfolioModule,
+  ],
+  controllers: [AnalyticsController],
+  providers: [AnalyticsService],
+  exports: [AnalyticsService],
+})
+export class AnalyticsModule {}
