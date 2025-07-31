05caeae22f61fa9cb73cadf7dd65ac5769f74b91
Arsenic <156888342+Arsenic-23@users.noreply.github.com>
2025-07-31 18:53:33 +0530

    Update trades.module.ts

--- src/trades/trades.module.ts
+++ src/trades/trades.module.ts
@@ -1,1 +1,17 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TradesController } from './trades.controller';
+import { TradesService } from './trades.service';
+import { Trade, TradeSchema } from './trades.schema';
+import { UsersModule } from '../users/users.module';
 
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Trade.name, schema: TradeSchema }]),
+    UsersModule,
+  ],
+  controllers: [TradesController],
+  providers: [TradesService],
+  exports: [TradesService],
+})
+export class TradesModule {}
