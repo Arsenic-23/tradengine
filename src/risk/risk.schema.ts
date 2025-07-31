75f47762d1a7ec91b337c18213b3befd9b5b0929
Arsenic <156888342+Arsenic-23@users.noreply.github.com>
2025-07-31 18:44:03 +0530

    Update risk.schema.ts

--- src/risk/risk.schema.ts
+++ src/risk/risk.schema.ts
@@ -1,1 +1,18 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
 
+export type RiskDocument = HydratedDocument<Risk>;
+
+@Schema({ timestamps: true })
+export class Risk {
+  @Prop({ required: true, unique: true })
+  user: string;
+
+  @Prop({ required: true })
+  maxLoss: number;
+
+  @Prop({ required: true })
+  maxDailyLoss: number;
+}
+
+export const RiskSchema = SchemaFactory.createForClass(Risk);
