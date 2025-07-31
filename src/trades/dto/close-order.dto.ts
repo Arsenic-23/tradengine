da777dbac661150a2cdf8bf51cc008bb2126af65
Arsenic <156888342+Arsenic-23@users.noreply.github.com>
2025-07-31 18:51:21 +0530

    Update close-order.dto.ts

--- src/trades/dto/close-order.dto.ts
+++ src/trades/dto/close-order.dto.ts
@@ -1,1 +1,10 @@
+import { IsString, IsNumber, IsOptional } from 'class-validator';
 
+export class CloseOrderDto {
+  @IsString()
+  tradeId: string;
+
+  @IsOptional()
+  @IsNumber()
+  exitPrice?: number;
+}
