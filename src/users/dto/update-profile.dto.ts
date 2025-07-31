ef61b425751b1371b5ba451dbbbe0b9d0e13a235
Arsenic <156888342+Arsenic-23@users.noreply.github.com>
2025-07-31 20:29:29 +0530

    Update update-profile.dto.ts

--- src/users/dto/update-profile.dto.ts
+++ src/users/dto/update-profile.dto.ts
@@ -1,2 +1,16 @@
+import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
 
+export class UpdateProfileDto {
+  @IsOptional()
+  @IsString()
+  readonly name?: string;
+
+  @IsOptional()
+  @IsEmail()
+  readonly email?: string;
+
+  @IsOptional()
+  @IsString()
+  readonly profilePicture?: string;
+}
 
