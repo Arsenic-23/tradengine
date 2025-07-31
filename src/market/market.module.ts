4066e6ecc7a416e736dcd70acb163a5fdfdcfcfa
Arsenic <156888342+Arsenic-23@users.noreply.github.com>
2025-07-31 18:55:39 +0530

    Update market.module.ts

--- src/market/market.module.ts
+++ src/market/market.module.ts
@@ -1,1 +1,11 @@
+import { Module } from '@nestjs/common';
+import { MarketService } from './market.service';
+import { MarketController } from './market.controller';
+import { WebsocketGateway } from './websocket.gateway';
 
+@Module({
+  controllers: [MarketController],
+  providers: [MarketService, WebsocketGateway],
+  exports: [MarketService],
+})
+export class MarketModule {}
