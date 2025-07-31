3bd21fc4eea24218596b4912414298e313c4e81b
Arsenic <156888342+Arsenic-23@users.noreply.github.com>
2025-07-31 18:48:55 +0530

    Update update-portfolio.dto.ts

--- src/portfolio/dto/update-portfolio.dto.ts
+++ src/portfolio/dto/update-portfolio.dto.ts
@@ -1,1 +1,15 @@
+import { IsOptional, IsNumber } from 'class-validator';
 
+export class UpdatePortfolioDto {
+  @IsOptional()
+  @IsNumber()
+  balance?: number;
+
+  @IsOptional()
+  @IsNumber()
+  equity?: number;
+
+  @IsOptional()
+  @IsNumber()
+  marginUsed?: number;
+}
