62698a5195354f10c071efbfcc2fe0dfca40d82e
Arsenic <156888342+Arsenic-23@users.noreply.github.com>
2025-07-31 18:51:08 +0530

    Update trade-history.dto.ts

--- src/trades/dto/trade-history.dto.ts
+++ src/trades/dto/trade-history.dto.ts
@@ -1,1 +1,15 @@
+import { IsOptional, IsString } from 'class-validator';
 
+export class TradeHistoryDto {
+  @IsOptional()
+  @IsString()
+  symbol?: string;
+
+  @IsOptional()
+  @IsString()
+  fromDate?: string;
+
+  @IsOptional()
+  @IsString()
+  toDate?: string;
+}
