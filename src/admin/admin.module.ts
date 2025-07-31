7b98048ae0e1182d59baa32e757be99a587f62b7
Arsenic <156888342+Arsenic-23@users.noreply.github.com>
2025-07-31 18:41:28 +0530

    Update admin.module.ts

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,1 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AdminService } from './admin.service';
+import { AdminController } from './admin.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from '../users/user.schema';
+import { Trade, TradeSchema } from '../trades/trades.schema';
+import { Portfolio, PortfolioSchema } from '../portfolio/portfolio.schema';
 
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: Trade.name, schema: TradeSchema },
+      { name: Portfolio.name, schema: PortfolioSchema },
+    ]),
+  ],
+  controllers: [AdminController],
+  providers: [AdminService],
+  exports: [AdminService],
+})
+export class AdminModule {}
