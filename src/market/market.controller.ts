046db58ea2168683b94779ba3c772fe8d7c861aa
Arsenic <156888342+Arsenic-23@users.noreply.github.com>
2025-07-31 18:55:18 +0530

    Update market.controller.ts

--- src/market/market.controller.ts
+++ src/market/market.controller.ts
@@ -1,1 +1,17 @@
+import { Controller, Get, Query } from '@nestjs/common';
+import { MarketService } from './market.service';
 
+@Controller('market')
+export class MarketController {
+  constructor(private readonly marketService: MarketService) {}
+
+  @Get('symbols')
+  getAvailableSymbols() {
+    return this.marketService.getAvailableSymbols();
+  }
+
+  @Get('price')
+  getPrice(@Query('symbol') symbol: string) {
+    return this.marketService.getPrice(symbol);
+  }
+}
