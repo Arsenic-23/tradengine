620e1735a14150c82ca237e2925d71365af90f9a
Arsenic <156888342+Arsenic-23@users.noreply.github.com>
2025-07-31 18:37:53 +0530

    Update logger.service.ts

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,1 +1,24 @@
+import { Injectable, Logger as NestLogger } from '@nestjs/common';
 
+@Injectable()
+export class LoggerService extends NestLogger {
+  log(message: string, context?: string) {
+    super.log(`[INFO] ${message}`, context);
+  }
+
+  error(message: string, trace?: string, context?: string) {
+    super.error(`[ERROR] ${message}`, trace, context);
+  }
+
+  warn(message: string, context?: string) {
+    super.warn(`[WARN] ${message}`, context);
+  }
+
+  debug(message: string, context?: string) {
+    super.debug(`[DEBUG] ${message}`, context);
+  }
+
+  verbose(message: string, context?: string) {
+    super.verbose(`[VERBOSE] ${message}`, context);
+  }
+}
