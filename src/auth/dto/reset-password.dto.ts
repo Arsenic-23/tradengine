5be8872fd1cf5befef06558af3be2421c6c99208
Arsenic <156888342+Arsenic-23@users.noreply.github.com>
2025-07-31 20:35:34 +0530

    Update reset-password.dto.ts

--- src/auth/dto/reset-password.dto.ts
+++ src/auth/dto/reset-password.dto.ts
@@ -1,1 +1,16 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
+export class ResetPasswordDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  @IsNotEmpty()
+  newPassword: string;
+
+  @IsString()
+  @IsNotEmpty()
+  token: string;
+}
