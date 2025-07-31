d1e1c0142c08be73ad0c92f0b93d4bef90d84714
Arsenic <156888342+Arsenic-23@users.noreply.github.com>
2025-07-31 20:29:11 +0530

    Update change-password.dto.ts

--- src/users/dto/change-password.dto.ts
+++ src/users/dto/change-password.dto.ts
@@ -1,1 +1,10 @@
+import { IsString, MinLength } from 'class-validator';
 
+export class ChangePasswordDto {
+  @IsString()
+  readonly currentPassword: string;
+
+  @IsString()
+  @MinLength(6)
+  readonly newPassword: string;
+}
