844786b9f96c1723f94f931b484879287be8cd28
Arsenic <156888342+Arsenic-23@users.noreply.github.com>
2025-07-31 18:45:06 +0530

    Update notifications.schema.ts

--- src/notifications/notifications.schema.ts
+++ src/notifications/notifications.schema.ts
@@ -1,1 +1,18 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
 
+export type NotificationDocument = HydratedDocument<Notification>;
+
+@Schema({ timestamps: true })
+export class Notification {
+  @Prop({ required: true })
+  user: string;
+
+  @Prop({ required: true })
+  message: string;
+
+  @Prop({ default: false })
+  read: boolean;
+}
+
+export const NotificationSchema = SchemaFactory.createForClass(Notification);
