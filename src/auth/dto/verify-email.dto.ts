91fb9a468cfca3ef083d61b202a75ab4dd93cfc0
Arsenic <156888342+Arsenic-23@users.noreply.github.com>
2025-07-31 20:35:18 +0530

    Update verify-email.dto.ts

--- src/auth/dto/verify-email.dto.ts
+++ src/auth/dto/verify-email.dto.ts
@@ -1,1 +1,11 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
+export class VerifyEmailDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  verificationCode: string;
+}
