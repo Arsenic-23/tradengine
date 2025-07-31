00d85c57a6e669cfd91bef76e8a69f742490507f
Arsenic <156888342+Arsenic-23@users.noreply.github.com>
2025-07-31 19:16:43 +0530

    Update upload-kyc.dto.ts

--- src/kyc/dto/upload-kyc.dto.ts
+++ src/kyc/dto/upload-kyc.dto.ts
@@ -1,1 +1,34 @@
+import {
+  IsString,
+  IsNotEmpty,
+} from 'class-validator';
 
+export class UploadKycDto {
+  @IsString()
+  @IsNotEmpty()
+  fullName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  dob: string;
+
+  @IsString()
+  @IsNotEmpty()
+  address: string;
+
+  @IsString()
+  @IsNotEmpty()
+  idType: string;
+
+  @IsString()
+  @IsNotEmpty()
+  idNumber: string;
+
+  @IsString()
+  @IsNotEmpty()
+  frontImage: string;
+
+  @IsString()
+  @IsNotEmpty()
+  backImage: string;
+}
