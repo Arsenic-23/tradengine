4b6e658f16741de6405751299409bc6d6697b5ea
Arsenic <156888342+Arsenic-23@users.noreply.github.com>
2025-07-31 18:47:24 +0530

    Update leaderboard.controller.ts

--- src/leaderboard/leaderboard.controller.ts
+++ src/leaderboard/leaderboard.controller.ts
@@ -1,1 +1,12 @@
+import { Controller, Get, Query } from '@nestjs/common';
+import { LeaderboardService } from './leaderboard.service';
 
+@Controller('leaderboard')
+export class LeaderboardController {
+  constructor(private readonly leaderboardService: LeaderboardService) {}
+
+  @Get()
+  getLeaderboard(@Query('type') type: 'profit' | 'percent' = 'profit') {
+    return this.leaderboardService.getRankings(type);
+  }
+}
