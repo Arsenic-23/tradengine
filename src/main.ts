25e2ee1650f71e59789d5cbf0c0383835157ecae
Arsenic <156888342+Arsenic-23@users.noreply.github.com>
2025-07-31 21:22:30 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,40 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { LoggerService } from './logger/logger.service';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
+import { ConfigService } from './config/config.service';
+import * as cookieParser from 'cookie-parser';
 
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+
+  const logger = app.get(LoggerService);
+  app.useLogger(logger);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('APP_PORT') || 3000;
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  app.useGlobalFilters(new HttpExceptionFilter(logger));
+  app.useGlobalInterceptors(new LoggingInterceptor(logger));
+  app.use(cookieParser());
+
+  app.enableCors({
+    origin: configService.get<string>('FRONTEND_URL'),
+    credentials: true,
+  });
+
+  await app.listen(port);
+  logger.log(`Application is running on port ${port}`);
+}
+bootstrap();
