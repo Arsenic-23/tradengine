1b95d10692485ac02dfcf0806e77f6e55dbd5232
Arsenic <156888342+Arsenic-23@users.noreply.github.com>
2025-07-31 18:42:27 +0530

    Update risk.module.ts

--- src/risk/risk.module.ts
+++ src/risk/risk.module.ts
@@ -1,1 +1,13 @@
+import { Module } from '@nestjs/common';
+import { RiskService } from './risk.service';
+import { RiskController } from './risk.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Risk, RiskSchema } from './risk.schema';
 
+@Module({
+  imports: [MongooseModule.forFeature([{ name: Risk.name, schema: RiskSchema }])],
+  controllers: [RiskController],
+  providers: [RiskService],
+  exports: [RiskService],
+})
+export class RiskModule {}
